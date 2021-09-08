b224a00ee7b9199f9dd6dc136759304ec17623f6
ludike23 <ludike23@gmail.com>
2021-09-08 18:10:20 +0100

    upload

---
+++ src/routes/index.svelte
@@ -0,0 +1,67 @@
+<script>
+	import successkid from 'images/successkid.jpg';
+import { onMount } from 'svelte';
+const refreshLocation = () =>{
+
+}
+	onMount(()=>{
+		if(navigator.geolocation){
+			console.log(navigator);
+			navigator.geolocation.getCurrentPosition((position)=>{
+				alert(JSON.stringify(position.coords));
+				console.log(position);
+			})
+		}
+		else{
+			alert('No geolocation API on your browser F*** out')
+		}
+
+	})
+</script>
+
+<style>
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<h1>Great success!</h1>
+
+<figure>
+	<img alt="Success Kid" src="{successkid}">
+	<figcaption>Have fun with Sapper!</figcaption>
+</figure>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
